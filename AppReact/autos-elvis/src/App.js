bf9a226066ac283da9a79b673e1a3663211db80c
Juanscee~ <you@example.com>
2022-12-27 20:58:24 -0300

    Preentrega1+Vera

--- AppReact/autos-elvis/src/App.js
+++ AppReact/autos-elvis/src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Cartwidget from './components/Cartwidget/Cartwidget';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+    <div>
+      <header className='nav'>
+        <Navbar />
+        <Cartwidget />
+        <ItemListContainer greeting={'Hola gente!'} />
+        </header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          
     </div>
+    </>
   );
 }
 
---
+++ AppReact/autos-elvis/src/components/Cartwidget/Cartwidget.js
@@ -0,0 +1,13 @@
+import './Cartwidget.css'
+
+const Cartwidget = () => {
+    return (
+<div className='cart'>
+    <img className='cart' src= './images/cart.png' alt='Cartwidget'  />
+    <p className='num'>0</p>
+</div>
+
+    )
+}
+
+export default Cartwidget
